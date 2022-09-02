648cf273f00faed44e033f4cd5d4b78cd6169bc9
luckyvasugupta <vasuguptadevops@gmail.com>
2022-09-02 19:42:25 +0530

    Grade According to Marks

---
+++ Marks.py
@@ -0,0 +1,31 @@
+"""
+write a program to print grade obtained in a test.Take input from the user and print grade.
+
+
+90 < X = A
+80 < X = B
+70 < X = C
+60 < X = D
+50 < X = E
+40 < X = F
+30 < X = G
+"""
+
+
+x=int(input("My name is Vasu"))
+if (x>90):
+    print("A")
+elif (x>80):
+    print("B")
+elif (x>=70):
+    print("C")
+elif (x>=60):
+    print("D")
+elif (x>=50):
+    print("E")
+elif (x>=40):
+    print("F")
+else :
+    print("G")
+
+

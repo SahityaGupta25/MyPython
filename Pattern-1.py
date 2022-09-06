9280c535c491fd351c576dabd328b2ed8bc180c6
luckyvasugupta <vasuguptadevops@gmail.com>
2022-09-06 20:43:41 +0530

    Pattern 1

---
+++ Pattern-1.py
@@ -0,0 +1,14 @@
+'''
+ Write a Program to Print Pattern Like this
+ 
+ * * * *
+ * * * *
+ * * * *
+ * * * *
+
+ '''
+
+for i in range(1,5):
+    for j in range(1,5):
+        print("*",end='')
+    print()

4f705bf37629ed20863059ca1515d14fd0fb7225
luckyvasugupta <vasuguptadevops@gmail.com>
2022-09-07 15:56:23 +0530

    Program to print 10 odd natural no. in reverse order

---
+++ ODD Natural Reverse.py
@@ -0,0 +1,9 @@
+# Write a Python Program to Print First Odd Natural Numbers in Reverse Order.
+#  -------------------------------------------------------------------------
+
+
+n=19
+i=1
+while (i<=n):
+    print(n,end=' ')
+    n-=2

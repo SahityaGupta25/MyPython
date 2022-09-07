0e05705aaa4a7dd56991fffc9708df0275cdd9d6
luckyvasugupta <vasuguptadevops@gmail.com>
2022-09-07 18:51:09 +0530

    Program to find squares of N Numbers

---
+++ Squares of N Numbers.py
@@ -0,0 +1,14 @@
+# Write a Python Program to Print Squares of N Numbers.
+# -----------------------------------------------------
+
+
+
+
+
+n=int(input("Enter a No."))
+sq=0
+i=1
+while (i<=n):
+    sq=i**2
+    print(sq)
+    i+=1

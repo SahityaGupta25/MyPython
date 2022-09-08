5cb90327cd1e723538a643b42d98b5b1ab7a8d46
luckyvasugupta <vasuguptadevops@gmail.com>
2022-09-08 11:07:10 +0530

    Print avg of 2 no. entered y user

---
+++ Avgof2no.py
@@ -0,0 +1,13 @@
+# Write a Program to Calculate Avg of Entered two Numbers.
+# --------------------------------------------------------
+
+
+
+
+
+a=int(input("Ente Value of a"))
+b=int(input("Ente Value of b"))
+avg=(a+b)//2
+print(avg)
+
+

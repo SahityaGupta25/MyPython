37629cbdcb0ff1b5edd3d7121187856fdd09a3f9
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-09-22 12:16:27 +0530

    List Programs of Python

---
+++ Lists/1.py
@@ -0,0 +1,4 @@
+# 1. Write a python script to store multiple items in a single variable ( Items are “Java”,“Python”, “SQL”, “C” ) using list.
+
+x=["Java","Python","SQL","C"]
+print(x)
---
+++ Lists/2.py
@@ -0,0 +1,5 @@
+# 2. Write a python script to get the data type of a list.
+# ==========================================================
+
+x=["Java","Python","SQL","C"]
+print(type(x))

aaff5757f2aa07cdc2daf1dad39e3ab1e1adcc4a
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-09-23 15:42:43 +0530

    Lists

---
+++ Lists/10.py
@@ -0,0 +1,5 @@
+# 10. Write a Python script to create a list, where each element of the list is a digit of a given number.
+# =====================================================================================
+
+n=list(input("Enter a Number\t"))
+print(n)

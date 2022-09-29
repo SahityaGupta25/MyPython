44a6730db2fac3c050beea08d71958b32d33789c
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-09-29 09:53:56 +0530

    Strings

---
+++ Strings/3.py
@@ -0,0 +1,3 @@
+# 3. Write a python script to Get the characters from position 2 to position 5 (Given Strin “Hello Learners” using the slice syntax) 
+x="Hello Learners"
+print(x[2:6])
---
+++ Strings/String to list.py
@@ -0,0 +1,4 @@
+# Converting from string to list also adding while Converting
+
+x="123456"
+print(sum(int(e) for e in x))

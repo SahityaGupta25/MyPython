931ddd31ecb48bb2dc16404110f043631ec5b4e3
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-09-29 09:21:28 +0530

    Strings Problems

---
+++ Strings/1.py
@@ -0,0 +1,6 @@
+# 1. Write a python script to create a String in 3 different possible ways
+# ==========================================================================
+
+print('''My name is Mathew''')
+print('My name is Mathew')
+print("My name is Mathew")
---
+++ Strings/2.py
@@ -0,0 +1,6 @@
+# 2. Write a python script to Get the characters from the start to position 5 (Given String “iNeuron” using the slice syntax)
+# =======================================================================================
+
+
+x="iNeuron"
+print(x[0:6])

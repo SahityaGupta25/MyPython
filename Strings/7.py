3f4a4f48f4585787a9fe9d1ff078397bb4243b97
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-09-30 08:27:34 +0530

    Strings

---
+++ Strings/7.py
@@ -0,0 +1,9 @@
+# 7. Write a python script to determine whether a string contains a specific substring.
+# ======================================================================================
+
+
+
+
+
+x= "jai bhole ki jai shree ram"
+print(x.find("ram"))

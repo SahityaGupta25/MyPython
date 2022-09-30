fa029d8cd1277eba45da63d299e41dc079af084e
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-09-30 10:56:41 +0530

    Strings

---
+++ Strings/6.py
@@ -0,0 +1,10 @@
+# 6. Write a python script to reverse a String. (“iNeuron”)
+# ============================================================================
+
+
+
+a="iNeuron"
+i=6
+while (i>=0):
+    print(a[i],end='')
+    i-=1

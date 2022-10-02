a1c31477a13310726fdffc1686ade93e54eb5037
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-10-02 18:47:31 +0530

    Add files via upload

--- Input/4.py
+++ Input/4.py
@@ -1,6 +1,6 @@
 # 4. Write a python script which takes the radius from the user and display area of a circle.
-
+# ========================================================================================
 pie=3.14
-r=int(input("Enter Value of Radius\t"))
+r=float(input("Enter Value of Radius\t"))
 x=pie*r**2
 print("Area of Circle is =",x)

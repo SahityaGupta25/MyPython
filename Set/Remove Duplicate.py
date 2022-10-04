86b07d0703825639f78243cbf071a5b056897dd8
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-10-04 19:14:57 +0530

    Set

---
+++ Set/Remove Duplicate.py
@@ -0,0 +1,6 @@
+# Remove Duplicate Values from List . Result should be in List.
+# ===============================================================
+
+x=[1,2,3,0,9,8,7,6,5,4,3,2,1,5,6,7,8,9,0]
+y=list(set(x))
+print(y)

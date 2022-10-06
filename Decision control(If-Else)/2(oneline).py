33bc15e91502de2657520dda5a06d9263bf376ba
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-10-06 11:43:28 +0530

    One Line 2 Problem

---
+++ Decision control(If-Else)/2(oneline).py
@@ -0,0 +1,12 @@
+'''
+2. Write a python script to check whether a given number is divisible by 5 or not
+-----------------------------------------------------------------------
+
+x=int(input("Enter a Number\t"))
+if (x%5==0):
+    print("Divisible By 5")
+else :
+    print("Not Divisible by 5")
+'''
+# Answer in one Line
+print("Number is Divisible by 5 " if int(input("Enter a Number"))%5==0 else "Number is Not Divisible")

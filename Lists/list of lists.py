8997a56aa4f25fa7cb7fc3469720a2e993fc03ad
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-10-07 11:45:56 +0530

    List of Lists

---
+++ Lists/list of lists.py
@@ -0,0 +1,9 @@
+# List of Lists
+# ====================================
+
+
+x=[[1,2,3],[4,5,6],[7,8,9]]
+# Accessing 2nd Index list in x
+print(x[2])
+# Accessing element of 0th list in x
+print(x[0][1])

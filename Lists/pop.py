bf911de50ceeb08979cb441110542215efbf9ae3
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-10-08 15:52:33 +0530

    List

---
+++ Lists/pop.py
@@ -0,0 +1,9 @@
+# Pop Attribute in list
+# =================================================
+
+
+x=[1,2,3,4,5,6,7]
+y=x.pop()
+print(x)
+# If pop Brackets are blanks it will remove the last index of list and also returns it
+# If pop Brackets is filled with Index No. it will remove element from that index also return it.

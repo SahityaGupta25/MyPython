fbd1fa495cdcf90f230b87a8b7e225b1391bd8e4
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-22 18:20:06 +0530

    7,8

--- Lambda & Recursion/8.py
+++ Lambda & Recursion/8.py
@@ -1,24 +1,8 @@
-# 8. Create an endless iterator using generator method to produce Square numbers
+# 8. Write a recursive python function to print cubes of first N natural numbers
 
+def cb(n):
+    if n>0:
+        cb(n-1)
+        print(n**3)
 
-def sq():
-    i=1
-    while True:
-        yield i**2
-        i+=1
-        
-it=sq()
-
-
-l1=[]
-while True:
-    ans=input("Choose between 'y' & 'n'\t")
-    if ans=='y':
-        x=next(it)
-        print(x)
-        y=l1.append(x)
-    elif ans=='n':
-        print(l1)
-        break
-
-
+cb(4)

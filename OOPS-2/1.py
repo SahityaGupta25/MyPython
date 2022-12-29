e22e98323f00ccf57a8eefe4b93aa6f497a7712d
SahityaGupta25 <99082422+SahityaGupta25@users.noreply.github.com>
2022-12-29 08:02:20 +0530

    OOPS-2

---
+++ OOPS-2/1.py
@@ -0,0 +1,11 @@
+# 1. Write a python program to create a user class with 3 properties : name, age, email.
+
+class user:
+    pass
+
+x=user()
+x.name='Mathew'
+x.age=16
+x.email="hsagdf@gmail.com"
+
+print(x.name,x.age,x.email)
